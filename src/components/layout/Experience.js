c283f8723a2f6abb10e33edf05284c060e681c63
Lucas Su <lijie.su93@gmail.com>
2024-01-23 17:53:35 +0800

    add @media for bg-image

--- src/components/layout/Experience.js
+++ src/components/layout/Experience.js
@@ -16,7 +16,7 @@ export default function Experience() {
     return (
         <section class="container pt-4" id="experience" style={{ maxWidth: "1200px" }}>
             <div className="container " >
-                <h2 class="text-shorter fw-bold">Experience</h2>
+                <h2 class="text-header">Experience</h2>
                 <div class="tm-main">
                     <div className="tm-header mb-2">
                         <span className="text-light p-2 px-3 rounded" style={{ backgroundColor: "#333" }}>
--- src/components/layout/Skills.js
+++ src/components/layout/Skills.js
@@ -12,7 +12,7 @@ export default function Skills() {
     return (
         <section class="container pt-4 " id="skills" style={{ maxWidth: "1200px" }}>
             <div className="container ">
-                <h2 class="text-shorter fw-bold">Skills</h2>
+                <h2 class="text-header">Skills</h2>
                 <div class="row">
                     {skillCard}
                 </div>
